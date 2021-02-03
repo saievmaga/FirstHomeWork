6a041155167f2a15e62293bb4d8f6792c81bc0dd
Saiev Magomed <saievmuhammad@mail.ru>
2021-02-04 00:15:20 +0300

    Lesson6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,27 @@
 
 public class Main {
 
-
     public static void main(String[] args) {
-        Employee[] empArr = new Employee[5];
-        empArr[0] = new Employee("Иванов Иван", "Инженер", "ivanovivan@mail.ru", "89280011010", "30000", 30);
-        empArr[1] = new Employee("Рубанов Алекс", "Механик", "ivanovivan@mail.ru", "89286665555", "35000", 39);
-        empArr[2] = new Employee("Шуткин Сергей", "Электрик", "ivanovivan@mail.ru", "89296644897", "25000", 41);
-        empArr[3] = new Employee("Штыркин Николай", "Сварщик", "ivanovivan@mail.ru", "89633218944", "40000", 43);
-        empArr[4] = new Employee("Качалов Василий", "Перевозчик", "ivanovivan@mail.ru", "89659574477", "35000", 35);
-        for (Employee employee : empArr) {
-            if (employee.getAge() > 40) {
-                employee.showInfo();
-            }
-        }
+      showInfo();
+    }
+
+    static void showInfo() {
+        Cat cat = new Cat();
+        cat.run(200);
+        cat.swim(150);
+
+        Dog dog = new Dog();
+        dog.run(26);
+        dog.swim(60);
+
+        Cat murzik = new Cat(10, 15);
+        murzik.runWithDistance(11);
+
+        Dog muhtar = new Dog(600, 10);
+        muhtar.runWithDistance(500);
+        muhtar.swimWithDistanse(10);
+
+        System.out.println("Вы создали " + Cat.catCount + " котов");
+        System.out.println("Вы создали " + Dog.dogCount + " собак");
     }
 }
